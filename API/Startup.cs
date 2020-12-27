8dc369b3edea4a15e5ace6adad64f40f0e453dcd
Sai Srikar Patibanda <saisrikar.net@gmail.com>
2020-12-27 06:18:01 -0500

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -59,6 +60,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
